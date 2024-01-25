8265657768f872fd5a99cb69a4e60a0557ec7206
lukagoderdzishvili <goderdzishvili21@gmail.com>
2024-01-25 18:22:05 +0400

    fix background position

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -23,14 +23,27 @@ export default class MainScene extends Phaser.Scene{
 
 
     private _addBackground(): void{
+        const width: number = (innerWidth / 1024);
+        const height: number = (innerHeight / 2000)
+        const scale: number = Math.min(width, height);
         this._background = this.add
-        .image(-590, 0, Configs.background.texture)
+        .image(0, 0, Configs.background.texture)
         .setOrigin(0, 0)
-        .setScale(0.4);
+        .setScale(scale);
+    }
+
+    private _resizeBackground(): void{
+        const width: number = (innerWidth / 1024);
+        const height: number = (innerHeight / 2000)
+        const scale: number = Math.min(width, height);
+        this._background
+        ?.setPosition(0, 0)
+        ?.setScale(scale);
     }
 
     public onScreenChange(): void{
         console.log('resize event');
+        this._resizeBackground();
         this._levelContainer?.onScreenChange();
         this._avatarContainer?.onScreenChange();
     }
